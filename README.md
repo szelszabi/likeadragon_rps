3582bf2205508ccbee141962595117feab7b8ba7
Szél Szabolcs <szel.szabolcs02@gmail.com>
2023-03-20 22:42:33 +0100

    All ready

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 <h1>Szél Kő Papír Olló Játéka</h1><br>
-A lore az, hogy Goro Majima párbajra hívta Kiryu Kazumát, nem is akármilyenre, egy BO3-as Kő Papír Olló meccsre.<br>
+A lore az, hogy Goro Majima párbajra hívta Kiryu Kazumát, nem is akármilyenre, egy FT11-es(First to 11) Kő Papír Olló meccsre.<br>
 Ez a gigantikus méretű megmérettetés végre eldönti Kamurocho városának a legnevesebb yakuzáját!<br>
 Ki fog győzni? A vakmerő Dojima Sárkánya, vagy Shimano Vad Kutyája. Minden a TE kezedben van, aki Kiryu Kazuma szerepét fogja betölteni ebben a csatában.
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var majimaCounter = 0;
 var kiryuCounter = 0;
 
+var Kpick = document.getElementById("i0");
+var Mpick = document.getElementById("i3");
+
 document.getElementById("scoreKiryu").innerHTML = kiryuCounter;
 document.getElementById("scoreMajima").innerHTML = majimaCounter;
 
@@ -10,6 +13,8 @@ function reset()
     kiryuCounter = 0;
     document.getElementById("scoreKiryu").innerHTML = kiryuCounter;
     document.getElementById("scoreMajima").innerHTML = majimaCounter;
+    document.getElementById("MajimaWon").style.display = "none";
+    document.getElementById("KiryuWon").style.display = "none";
 }
 
 function menu_show()
@@ -17,6 +22,9 @@ function menu_show()
     var menu = document.getElementById("menu");
     var lore = document.getElementById("lore");
     var game = document.getElementById("game");
+    var go = document.getElementById("game_over");
+
+    go.style.display = "none";
     lore.style.display = "none";
     game.style.display = "none";
     menu.style.display = "block";
@@ -35,7 +43,74 @@ function game_show()
 {
     var menu = document.getElementById("menu");
     var game = document.getElementById("game"); 
+    var go = document.getElementById("game_over");
+    go.style.display = "none";
     menu.style.display = "none";
     game.style.display = "block";
 }
 
+function randint(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function game_over()
+{
+    var go = document.getElementById("game_over");
+    var game = document.getElementById("game"); 
+    game.style.display = "none";
+    go.style.display = "block";
+    if (kiryuCounter == 11) 
+    {
+        document.getElementById("KiryuWon").style.display = "block";  
+    }
+    else
+    {
+        document.getElementById("MajimaWon").style.display = "block";
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}                      // r    s    p
+async function match(Kiryu) // 0 -> 1 -> 2
+{                     // ^---------ˇ
+    Mpick.style.display = "none";
+    Kpick.style.display = "none";
+    const arr = [0,1,2];
+    var result = document.getElementById("result");
+    result.innerHTML = "";
+    var Majima = randint(3, 5);
+    Kpick = document.getElementById("i"+Kiryu.toString());
+    Mpick = document.getElementById("i"+Majima.toString());
+    Kpick.style.display = "block";
+    Mpick.style.display = "block";
+    if (Majima%3 == Kiryu) 
+    {
+        result.innerHTML = "Draw!";
+    }
+    else if (arr[(Kiryu+1)%3] == arr[Majima%3])
+    {
+        kiryuCounter++;
+        document.getElementById("scoreKiryu").innerHTML = kiryuCounter;
+        result.innerHTML = "Kiryu won!";
+    }
+    else 
+    {
+        majimaCounter++;
+        document.getElementById("scoreMajima").innerHTML = majimaCounter;
+        result.innerHTML = "Majima won!";
+    }
+    await sleep(3000);
+    if(majimaCounter == 11 || kiryuCounter == 11)
+    {
+        game_over();
+        return;
+    }
+}
+
+function retry()
+{
+    reset();
+    game_show();
+}
